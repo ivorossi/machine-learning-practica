b5a1829bcfa95ffb1513026b7a70fe3ccd2aee20
ivo <rossiivomartin@gmail.com>
2024-11-30 22:11:36 -0300

    utils image

--- src/main/python/gesturedetector/utils/download.py
+++ src/main/python/gesturedetector/utils/download.py
@@ -1,26 +1,41 @@
 import os.path
+from io import BytesIO
 import requests
 import imghdr
+from PIL.Image import Image
+
 from src.main.python.gesturedetector.config.configurations import Config
 
 download_path = Config.get_config()['download_folder']
 
 
-def download_image_with_format(url: str, name: str):
+def download_image(url: str):
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()
+        return response.content
+    except requests.exceptions.RequestException:
+        return None
 
-        image_bytes = response.content
-        image_format = imghdr.what(None, h=image_bytes)
-        if not image_format:
-            print("Error: Unable to determine the image format.")
-            return
 
-        file_path = os.path.join(download_path, f"{name}.{image_format}")
+def get_image_format(image_bytes):
+    image_format = imghdr.what(None, h=image_bytes)
+    if not image_format:
+        image_format = 'undefined'
+    return image_format
+
+
+def is_integrity_correct(image_byte):
+    try:
+        with Image.open(BytesIO(image_byte)) as img:
+            img.verify()
+        return True
+    except OSError:
+        return False
+
+
+def save_image(image_bytes, name):
+    if image_bytes and is_integrity_correct(image_bytes):
+        file_path = os.path.join(download_path, f"{name}.{get_image_format(image_bytes)}")
         with open(file_path, "wb") as file:
             file.write(image_bytes)
-
-        print(f"Image downloaded and saved as: {file_path}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading the image: {e}")
